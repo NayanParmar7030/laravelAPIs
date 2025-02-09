e090c7c9b7e10dbbc2ae2ee548d10560e1fe7cda
Nayan Parmar <120802567+Nayan7030@users.noreply.github.com>
2025-02-09 23:14:58 +0530

    Added code for APIs

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(Request $request){
+
+        $validateUser = Validator::make($request->all,[
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        if($validateUser->fails()){
+            return response()->json(
+                [
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validateUser->errors()->all
+            ], 401);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User created successfully',
+            'data' => $user
+        ]);
+    }
+
+    public function login(Request $request){
+        $validateUser = Validator::make($request->all,[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validateUser->fails()){
+            return response()->json(
+                [
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validateUser->errors()->all
+            ], 401);
+        }
+
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $user = Auth::user();
+            $token = $user->createToken('API Token')->plainTextToken;
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
+                'data' => $user,
+                'token' => $token
+            ]);
+        }
+        else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+            
+    }
+
+    public function logout(Request $request){
+        $user = $request->user();
+        $user->tokens()->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User logged out successfully'
+        ]);
+    }
+}
